21bcea823e26255abfdcbc787c8ce8ec2ffd2d7c
siemens-c2c <joy_mon78@yahoo.co.in>
2020-11-03 16:34:20 +0530

    react app updated with handler

--- codes/day-2/react-props-state-demo/src/App.js
+++ codes/day-2/react-props-state-demo/src/App.js
@@ -3,19 +3,18 @@ import React from 'react'
 import Welcome from './Welcome';
 
 const App = () => {
-  const messageValue = 'welcome to react js'
-  // const ele = Welcome({ message: '', data: 10 })
+  let messageValue = 'welcome to react js'
+  const changeMessageHandler = () => {
+    messageValue = 'Welcome to the world of React JS'
+  }
   return (
     <div className="App">
+      <button onClick={changeMessageHandler}>Change Message</button>
+      <br />
+      <br />
       <Welcome message={messageValue} data={10} />
     </div>
   );
 }
 
 export default App;
-/**
- * {
- *   message:'welcome to react js',
- *   data:10
- * }
- */
